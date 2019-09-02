ad1191863f1335768b46b2aef321d7fb01c144ec
VuTran <vutran.270797@gmail.com>
2019-09-02 21:24:48 +0700

    Updates

---
+++ precache-manifest.2c5e15a583e4612f0685e744ad5dca1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e66390421a1b5108effb8a1fb99d3ec",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "710170e689ef3d9b1b3a",
+    "url": "/monster-rolodex/static/css/main.de16b1e9.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monster-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "710170e689ef3d9b1b3a",
+    "url": "/monster-rolodex/static/js/main.a57caa94.chunk.js"
+  },
+  {
+    "revision": "2a0841746d3b50a5d128",
+    "url": "/monster-rolodex/static/js/runtime~main.cc3a76ac.js"
+  }
+]);
